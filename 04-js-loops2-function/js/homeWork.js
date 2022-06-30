00bb91400ef9c6063262b5e338e3eb1c181e8abc
Elazar-Goldman <elazargoldman6@gmail.com>
2022-06-30 19:25:45 +0300

    With HW boom

---
+++ 04-js-loops2-function/js/homeWork.js
@@ -0,0 +1,36 @@
+// When we use let the SCOPE of the variable is local
+// When we use var the SCOPE of the variable is global
+
+// gloabel becaues it was called outside of any scope.
+let msg = "Ohhhh my g-d!!!!!!!!!!!   there is a boom here";
+
+let flag = 0;
+
+for (let x = 1; x <= 100; x++) {
+
+  if (x % 7 === 0 || x.toString().includes("7")) {
+
+    if (flag === 1) {
+
+      console.log("This is a double boom");
+
+    } else {
+
+      console.log(msg);
+    }
+
+    flag = 1;
+  } else {
+    console.log(x);
+
+    flag = 0;
+  }
+}
+
+console.log(msg);
+// console.log("Loook over here !!!!!: " + x);
+
+
+let souts = ["&#9824", "&#9827", "&#9829;", "&#9830;"];
+let suitNames = ["Spade", "Club", "Hart", "Diamond"];
+
