8d7a21c61d33d4cb3714a4e361b4216c9dd31068
Elazar-Goldman <elazargoldman6@gmail.com>
2022-07-04 18:00:56 +0300

    class 5

--- 04-js-loops2-function/js/homeWork.js
+++ 04-js-loops2-function/js/homeWork.js
@@ -7,15 +7,10 @@ let msg = "Ohhhh my g-d!!!!!!!!!!!   there is a boom here";
 let flag = 0;
 
 for (let x = 1; x <= 100; x++) {
-
   if (x % 7 === 0 || x.toString().includes("7")) {
-
     if (flag === 1) {
-
       console.log("This is a double boom");
-
     } else {
-
       console.log(msg);
     }
 
@@ -30,7 +25,12 @@ for (let x = 1; x <= 100; x++) {
 console.log(msg);
 // console.log("Loook over here !!!!!: " + x);
 
-
 let souts = ["&#9824", "&#9827", "&#9829;", "&#9830;"];
 let suitNames = ["Spade", "Club", "Hart", "Diamond"];
 
+for (let x = 0; x < suitNames.length; x++) {
+  console.log(suitNames[x]);
+  for (i = 1; i <= 13; i++) {
+    console.log(i +" "+ suitNames[x]);
+  }
+}
