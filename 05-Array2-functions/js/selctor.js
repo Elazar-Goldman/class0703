7496d1509d7e74a7fbdd5ed55b0dd9b786b1b543
Elazar-Goldman <elazargoldman6@gmail.com>
2022-07-04 20:25:46 +0300

    with ul li

--- 05-Array2-functions/js/selctor.js
+++ 05-Array2-functions/js/selctor.js
@@ -9,3 +9,23 @@ myClass[1].style.backgroundColor = "green";
 let myRoot = document.getElementById("root");
 
 myRoot.innerHTML = "<ul><li>Isreal</li><li>USA</li><li>Argantina</li></ul>";
+
+//I am teaching you one method that pretty soon we will overright
+// I must show you this BECAUSE YOU WILL bump in to it;
+// This is a bit hard because of the canctanation
+let frot = "Malon";
+
+let under = document.getElementById("under-div");
+// when you want to inject JS into a string you do the following "+<JS> +"
+under.innerHTML = "<ul>";
+under.innerHTML += '<li class="frot">Orange </li>';
+under.innerHTML += "<li class='frot'>Mango</li>";
+under.innerHTML += "<li class=\"frot\">Apple</li>";
+under.innerHTML += "<li>Lemon</li>";
+under.innerHTML += "<li>Tomato</li>";
+under.innerHTML += "<li>" + frot + "</li>";
+under.innerHTML += "</ul>";
+
+let myCars = ["BMW", "Mazda", "Fiat", "Audi"]
+
+let myColor = ["hotPink", "red", "blue", "green"]
