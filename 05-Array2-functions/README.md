fd8e1a3717c3fa32c36bdb33bd69a370e1c9ab6f
Elazar-Goldman <elazargoldman6@gmail.com>
2022-07-04 20:11:39 +0300

    inners

---
+++ 05-Array2-functions/README.md
@@ -0,0 +1,17 @@
+## A document????!!!!!
+
+## selectors :
+
+-  id
+- docuent.getElemntById('title')
+
+- class 
+- docuent.getElemntsByClassName('inner-div')
+
+##  two very importent commends
+
+- innerHTML ==> Writing in to the document
+
+-<Anklits> will be to show that you have a choice to make
+
+- style.<attribute> = "<value>"
---
+++ 05-Array2-functions/js/selctor.js
@@ -0,0 +1,11 @@
+let elem = document.getElementById("title");
+
+let myClass = document.getElementsByClassName("inner-div");
+
+elem.innerHTML += " No Sh!!!!!! This is so Cool";
+
+myClass[1].style.backgroundColor = "green";
+
+let myRoot = document.getElementById("root");
+
+myRoot.innerHTML = "<ul><li>Isreal</li><li>USA</li><li>Argantina</li></ul>";
