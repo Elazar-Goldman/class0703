1c675b264bd3ed5fe2e8e18d7ad92bb61ce25014
Elazar-Goldman <elazargoldman6@gmail.com>
2022-07-07 18:38:34 +0300

    Loop homeWork

---
+++ 06-function/js/homeWork.js
@@ -0,0 +1,23 @@
+let elem = document.getElementById("root");
+
+for (let x = 9; x > 0; x--) {
+    
+  if (x % 3 === 0) {
+    elem.innerHTML += '<div class="inner-div"></div>';
+  }
+  // coolecting all of the divs
+  let allDivs = document.getElementsByClassName("inner-div");
+  // this is taking care of the index
+  let index = 0;
+  if (x <= 6 && x > 3) {
+    index = 1;
+  } else if (x <= 3 && x > 0) {
+    index = 2;
+  } else {
+    index = 0;
+  }
+
+  allDivs[index].innerHTML += x;
+}
+
+let myDivs = document.getElementsByClassName("inner-div");
