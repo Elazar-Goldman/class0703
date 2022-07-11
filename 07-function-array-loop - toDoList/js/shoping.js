fe7fc17a2f7f3c752bb55bfcf76b43362e2f5c8e
Elazar-Goldman <elazargoldman6@gmail.com>
2022-07-11 20:22:21 +0300

    display list

--- 07-function-array-loop - toDoList/js/shoping.js
+++ 07-function-array-loop - toDoList/js/shoping.js
@@ -1,21 +1,43 @@
-let list = [];
+let list = ["Apple", "Tommato", "Orange"];
 
 function addItem() {
   // I can pick up the value of the input in one line
-  let newItem = document.querySelector("#item").value;
+  //   let newItem = document.querySelector("#item").value;
+
   // or I can have 2 steps one to pick up the html item, and one for the value
-  //   let newItem = document.querySelector("#item");
-  //   let val = newItem.value;
-  console.log(newItem);
-  list.push(newItem);
+  let newItem = document.querySelector("#item");
+  let val = newItem.value;
+  console.log(val);
+  list.push(val);
+  newItem.value = "";
 }
 
 function addVipItem() {
   // I can pick up the value of the input in one line
+  let vipItem = document.querySelector("#vipItem")
   let newVipItem = document.querySelector("#vipItem").value;
   // or I can have 2 steps one to pick up the html item, and one for the value
   //   let newItem = document.querySelector("#item");
   //   let val = newItem.value;
   console.log(newVipItem);
   list.unshift(newVipItem);
+  vipItem.value = "";
 }
+
+function dispList() {
+  let dispElem = document.querySelectorAll(".disp");
+  dispElem[0].innerHTML = "<ul>";
+  // for of Loop
+  for (let product of list) {
+    dispElem[0].innerHTML += "<li>" + product + "</li>";
+  }
+  dispElem[0].innerHTML += "</ul>";
+}
+
+// for (let x = 0; x < list.length; x++) {
+//   console.log(list[x]);
+// }
+// Intro to the for of loop
+// for (let product of list) {
+//   console.log(product);
+// }
