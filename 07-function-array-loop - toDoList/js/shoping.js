0cc4fe11aaa89dbeb9a24283b9d57b8b77b76559
Elazar-Goldman <elazargoldman6@gmail.com>
2022-07-11 18:59:17 +0300

    shoping list

--- 07-function-array-loop - toDoList/js/shoping.js
+++ 07-function-array-loop - toDoList/js/shoping.js
@@ -1,8 +1,21 @@
+let list = [];
+
 function addItem() {
-    // I can pick up the value of the input in one line
+  // I can pick up the value of the input in one line
   let newItem = document.querySelector("#item").value;
-  // or I can have 2 steps one to pick up the html item, and one for the value 
-//   let newItem = document.querySelector("#item");
-//   let val = newItem.value;
+  // or I can have 2 steps one to pick up the html item, and one for the value
+  //   let newItem = document.querySelector("#item");
+  //   let val = newItem.value;
   console.log(newItem);
+  list.push(newItem);
+}
+
+function addVipItem() {
+  // I can pick up the value of the input in one line
+  let newVipItem = document.querySelector("#vipItem").value;
+  // or I can have 2 steps one to pick up the html item, and one for the value
+  //   let newItem = document.querySelector("#item");
+  //   let val = newItem.value;
+  console.log(newVipItem);
+  list.unshift(newVipItem);
 }
