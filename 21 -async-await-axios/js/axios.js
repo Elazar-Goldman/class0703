ea94704c5ad0579af1eb99d5f86d626bd679def5
Elazar-Goldman <elazargoldman6@gmail.com>
2022-09-12 21:13:12 +0300

    axios two

--- 21 -async-await-axios/js/axios.js
+++ 21 -async-await-axios/js/axios.js
@@ -1,9 +1,17 @@
 let url = "http://fs1.co.il/bus/bitcoin.php";
+let url2 = "http://fs1.co.il/bus/tv.php";
 
 let doApi = async () => {
   let data = await axios.get(url);
   console.log(data.data);
-  
+
+  let myData = await axios({
+    method: "get",
+
+    url: url2,
+  });
+
+  console.log(myData);
 };
 
 doApi();
